ff15e85e6ed8a5758a109aa4a439be6d1796776d
Jack <jackgdryden@gmail.com>
2020-11-09 02:28:29 -0800

    commit on development

--- js/main.js
+++ js/main.js
@@ -2,9 +2,15 @@ function tabAnnouncements(tabElement){
     if(tabElement == document.getElementById("websiteAnnouncements")){
         document.getElementById("translatorAnnouncements").removeAttribute("class");
         document.getElementById("websiteAnnouncements").setAttribute("class", "active");
+        document.getElementById("announcementContent2").innerHTML =
+        "<div class='announcementTitle' id='announcementTitle2'><a href=''>Title</a></div><div class='announcementDetails' id='announcementDetails2'><p>Time</p><p>Author</p></div>";
+        //document.getElementById("announcementFeed2").innerHTML = "";
     }
     else{
         document.getElementById("websiteAnnouncements").removeAttribute("class");
         document.getElementById("translatorAnnouncements").setAttribute("class", "active");
+        document.getElementById("announcementContent2").innerHTML =
+         "<a href='www.google.com'>test</a>";
+        //document.getElementById("announcementFeed2").innerHTML = "";
     }
 }
